88a41c4d6740c45cbbb5c0e08227a6f0a116bd54
DELL <DELL@DESKTOP-N0ORAA2>
2017-12-19 13:16:45 +0700

    abc

--- QuanLyMuaCaFe/QuanLyMuaCaFe/frmLogin.cs
+++ QuanLyMuaCaFe/QuanLyMuaCaFe/frmLogin.cs
@@ -54,7 +54,7 @@ namespace QuanLyMuaCaFe
                     }
                 }
                 fView.ShowDialog();
-                this.Hide();
+    
             }
             else
             {
